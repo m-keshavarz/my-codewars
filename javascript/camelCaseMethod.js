2c30024768d56c8d5425a90ccd557838a9563801
m-keshavarz <m.k.dev@outlook.com>
2024-02-09 10:31:21 +0330

    add camelCase Method

---
+++ javascript/camelCaseMethod.js
@@ -0,0 +1,14 @@
+// Write a method (or function, depending on the language) that converts a string to camelCase, that is, all words must have their first letter capitalized and spaces must be removed.
+
+// Examples (input --> output):
+// "hello case" --> "HelloCase"
+// "camel case word" --> "CamelCaseWord"
+
+String.prototype.camelCase = function () {
+  return this.toString()
+    .split(" ")
+    .map((item) => item.charAt(0).toUpperCase().concat(item.slice(1)))
+    .join("");
+};
+
+console.log("hello world".camelCase());
