edcadd2ae737a88a6f6d3b2d1f02fb24e3632ce9
m-keshavarz <m.k.dev@outlook.com>
2024-03-15 12:11:48 +0330

    add last digit of a large number

---
+++ javascript/lastDigitOfaLargeNumber.js
@@ -0,0 +1,12 @@
+// Examples;
+// lastDigit(4n, 1n); // returns 4n
+// lastDigit(4n, 2n); // returns 6n
+// lastDigit(9n, 7n); // returns 9n
+// lastDigit(10n, 10000000000n); // returns 0n
+
+function lastDigit(n, m) {
+  
+  return 0n;
+}
+
+lastDigit('41n', '12n');
