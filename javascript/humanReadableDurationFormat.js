fcbdf563ee09764569ab5b3a10c01a5336b88091
m-keshavarz <m.k.dev@outlook.com>
2024-04-16 13:11:27 +0330

    add human readable duration format

---
+++ javascript/humanReadableDurationFormat.js
@@ -0,0 +1,73 @@
+// Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+
+// The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+
+// It is much easier to understand with an example:
+
+// * For seconds = 62, your function should return
+//     "1 minute and 2 seconds"
+// * For seconds = 3662, your function should return
+//     "1 hour, 1 minute and 2 seconds"
+// For the purpose of this Kata, a year is 365 days and a day is 24 hours.
+
+// Note that spaces are important.
+
+// Detailed rules
+// The resulting expression is made of components like 4 seconds, 1 year, etc. In general, a positive integer and one of the valid units of time, separated by a space. The unit of time is used in plural if the integer is greater than 1.
+
+// The components are separated by a comma and a space (", "). Except the last component, which is separated by " and ", just like it would be written in English.
+
+// A more significant units of time will occur before than a least significant one. Therefore, 1 second and 1 year is not correct, but 1 year and 1 second is.
+
+// Different components have different unit of times. So there is not repeated units like in 5 seconds and 1 second.
+
+// A component will not appear at all if its value happens to be zero. Hence, 1 minute and 0 seconds is not valid, but it should be just 1 minute.
+
+// A unit of time must be used "as much as possible". It means that the function should not return 61 seconds, but 1 minute and 1 second instead. Formally, the duration specified by of a component must not be greater than any valid more significant unit of time.
+
+const timeMaker = (identifier, text) => {
+  return identifier > 1
+    ? identifier + " " + text + "s"
+    : identifier > 0
+    ? identifier + " " + text
+    : "";
+};
+
+function formatDuration(sec) {
+  if (sec === 0) return "now";
+
+  const seconds = Math.floor((sec % 60) % 60);
+  const minutes = Math.floor((sec / 60) % 60);
+  const hours = Math.floor((sec / 60 / 60) % 24);
+  const days = Math.floor((sec / 60 / 60 / 24) % 365);
+  const years = Math.floor(sec / 60 / 60 / 24 / 365);
+
+  const secondsText = timeMaker(seconds, "second");
+  const minutesText = timeMaker(minutes, "minute");
+  const hoursText = timeMaker(hours, "hour");
+  const daysText = timeMaker(days, "day");
+  const yearsText = timeMaker(years, "year");
+
+  const resultArr = [
+    yearsText,
+    daysText,
+    hoursText,
+    minutesText,
+    secondsText,
+  ].filter((item) => item !== "");
+
+  let result = "";
+  for (let i = 0; i < resultArr.length; i++) {
+    result += resultArr[i];
+    if (i < resultArr.length - 2) {
+      result += ", ";
+    }
+    if (i === resultArr.length - 2) {
+      result += " and ";
+    }
+  }
+
+  return result;
+}
+
+console.log(formatDuration(132030240));
